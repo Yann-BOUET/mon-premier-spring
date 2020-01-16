7540c551721d4d9b3621c6b81bd8d3723d153e39
YannBOUET <yann-dbz@hotmail.fr>
2020-01-16 14:06:26 +0100

    initial commit.
    Vanilla Spring Boot project

---
+++ src/main/java/fr/valarep/monPremierSpring/MonPremierSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.valarep.monPremierSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonPremierSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonPremierSpringApplication.class, args);
+	}
+
+}
